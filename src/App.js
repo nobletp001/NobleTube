2868d525350f4d2161b67cc3c2fd5203fb67db27
Temitope <ojotemitopeidnoble10@gmail.com>
2021-08-31 11:14:48 -0700

    loading

--- src/App.js
+++ src/App.js
@@ -5,29 +5,32 @@ import Global from "./components/Global";
 const Header = React.lazy(() => import("./components/header"));
 const Watch = React.lazy(() => import("./components/Watch"));
 const NotFound = React.lazy(() => import("./components/NotFound"));
-const Loader =()=>{
-  return <div> <img src="loading.gif" class="img-fluid" alt="loading"></img></div>
-}
+
+// const Loader =()=>{
+//   return <div> <img src="loading.gif" class="img-fluid" alt="loading"></img></div>
+// }
 
 function App() {
   return (
     <div>
-      <Suspense fallback={<Loader />}>
-        <Global />
-        <Router>
-          <Switch>
-            <Route path="/" exact>
-              <Header />
-            </Route>
-            <Route path="/watch/:id">
-              <Watch />
-            </Route>
-            <Route>
-              <NotFound />
-            </Route>
-          </Switch>
-        </Router>
-      </Suspense>
+        <Suspense fallback={<div>Loading...</div>}>
+
+      <Global />
+      <Router>
+        <Switch>
+          <Route path="/" exact>
+            <Header />
+          </Route>
+          <Route path="/watch/:id">
+            <Watch />
+          </Route>
+          <Route >
+           <NotFound/>
+          </Route>
+        </Switch>
+      </Router>
+        </Suspense>
+
     </div>
   );
 }
